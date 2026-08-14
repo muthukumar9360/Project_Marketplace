35dd1367ffcf2a96eba03274721b71dd258d8b49
muthukumar9360 <perumpatayans@gmail.com>
2026-08-14 06:16:47 +0530

    Configure service worker with full Firebase config for background notifications

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -4,13 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 const firebaseConfig = {
-  // It's recommended to populate this from a config endpoint or query param,
-  // but for simplicity you can inject the sender ID here if you want.
-  // Using URL search params trick to pass config to SW if needed,
-  // Or just hardcode for your specific project if you prefer since these are public.
-  // We'll leave it mostly empty and rely on the frontend to handle foreground.
-  // Background will work if you add the sender ID below:
-  messagingSenderId: '856953400730' // The user will need to update this
+  apiKey: "AIzaSyDz8lepW8tDnWf6L70gWHiBas1gIZIJPUo",
+  authDomain: "marketplace-21f80.firebaseapp.com",
+  projectId: "marketplace-21f80",
+  storageBucket: "marketplace-21f80.firebasestorage.app",
+  messagingSenderId: "856953400730",
+  appId: "1:856953400730:web:fe24b9be48046c319c9e23"
 };
 
 if (firebase.messaging.isSupported()) {
